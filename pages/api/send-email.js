92090ebce50e0550e50a0cd18d56f32f4a249d16
camilitwo <camilo.igv@gmail.com>
2024-06-06 13:37:51 -0400

    add cors

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,8 +1,17 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+        res.status(200).end();
+        return;
+    }
+
     if (req.method !== 'POST') {
-        res.setHeader('Allow', ['POST']);
+        res.setHeader('Allow', ['POST', 'OPTIONS']);
         return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 
